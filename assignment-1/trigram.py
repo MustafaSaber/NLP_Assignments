dee09b8bb4fc4444d30feccb98c2667390e32230
Mostafa Saber <mostafa.saber3789@gmail.com>
2019-03-15 23:34:28 +0200

    Prediction was successfull

--- assignment-1/trigram.py
+++ assignment-1/trigram.py
@@ -55,52 +55,54 @@ def createdic(trigram):
 
 
 def p(uni_dictionary, bi_dictionary, tir_dictionary, trigram):
-    biFinaldic={}
+    bifinaldic={}
     finaldic = {}
 
     for (a, b, c) in trigram:
-        biFinaldic[(a,b)]=math.log(uni_dictionary[a]/len(uni_dictionary)) + math.log(bi_dictionary[(a, b)]/len(bi_dictionary))
+        bifinaldic[(a, b)] = math.log(uni_dictionary[a]/len(uni_dictionary)) + math.log(bi_dictionary[(a, b)]/len(bi_dictionary))
         finaldic[(a, b, c)] = math.log(uni_dictionary[a]/len(uni_dictionary)) + math.log(bi_dictionary[(a, b)]/len(bi_dictionary)) + \
                               math.log(tir_dictionary[(a, b, c)]/len(tir_dictionary))
         # finaldic[(a, b, c)] = uni_dictionary[a]/len(uni_dictionary) * bi_dictionary[(a, b)]/len(bi_dictionary) * \
         #                       tir_dictionary[(a, b, c)]/len(tir_dictionary)
-    return biFinaldic,finaldic
+    return bifinaldic, finaldic
+
+
+def test(word, bi, allprops):
+    secWord = ""
+    thirdWord = ""
+    prop1 = -1000
+    prop2 = -1000
+    for (a, b)in bi:
+        if a != word:
+            continue
+        if bi[(a, b)] > prop1:
+            prop1 = bi[(a, b)]
+            secWord = b
+    sent = word + " " + secWord
+    for (a, b, c) in allprops:
+        if a != word or b != secWord:
+            continue
+        if allprops[(a, b, c)] > prop2:
+            prop2 = allprops[(a, b, c)]
+            thirdWord = c
+    sent += " " + thirdWord
+    return sent
 
 
 if __name__ == '__main__':
-    document = readfile("tryarabic.txt")
+    document = readfile("courps.txt")
     tokens = tokenzdoc(document)
     trigram = ngram(tokens, 3)
 
     uni_dictionary, bi_dictionary, tir_dictionary = createdic(trigram)
-    bi,allprops = p(uni_dictionary, bi_dictionary, tir_dictionary, trigram)
-    print(uni_dictionary)
-    print(bi_dictionary)
-    print(tir_dictionary)
-    print(bi)
-    print(allprops)
+    bi, allprops = p(uni_dictionary, bi_dictionary, tir_dictionary, trigram)
+    # print(bi)
+    # print(allprops)
+    # enter the test word
+    x = test("اللجنة", bi, allprops)
+    print(x)
 
-def test (word):
-    secWord=""
-    thirdWord=""
-    prop1=-1000
-    prop2=-1000
-    for (a,b)in bi:
-        if (a != word):
-            continue
-        if (bi[(a,b)]>prop1):
-            prop1=bi[(a,b)]
-            secWord=b
-    sent=word+" "+secWord
-    for (a,b,c) in allprops:
-        if (a!=word or b!=secWord):
-            continue
-        if (allprops[(a,b,c)]>prop2):
-            prop2=allprops[(a,b,c)]
-            thirdWord=c
-    sent+=" "+thirdWord
-    return sent
-#************enter the test word
-x=test("")
-print(x)
+
+
+
 
