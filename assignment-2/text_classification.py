7c9609779967cfa90fd937e10fa417d5716ea168
Mostafa Saber <mostafa.saber3789@gmail.com>
2019-04-24 19:14:43 +0200

    split components

--- assignment-2/text_classification.py
+++ assignment-2/text_classification.py
@@ -6,12 +6,24 @@ from sklearn.decomposition import TruncatedSVD
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 
-def set_matrices(x, y, to_split, vectroizer_used):
+
+def set_matrices(x, y, to_split):
     y_train, y_test = y[:to_split], y[to_split:]
     x_train, x_test = x[:to_split], x[to_split:]
 
-    x_train, x_test = vectroizer_used.fit_transform(x_train), vectroizer_used.transform(x_test)
-    return x_train, y_train, x_test, y_test
+    def tf_idf():
+        tfid = TfidfVectorizer(lowercase=True, analyzer='word', stop_words='english')
+        return tfid, tfid.fit_transform(x_train), tfid.transform(x_test)
+
+    def se_sum_of_we():
+        lines = open("/assignment-2/glove.6B.100d.txt").readlines()
+        dic = {}
+        for i in lines:
+            values = i.split()
+            dic[values[:1]] = values[1:]
+
+    vec, x_train, x_test = tf_idf()
+    return vec, x_train, y_train, x_test, y_test
 
 
 def predict(text, model_used, vectroizer_used):
@@ -31,8 +43,7 @@ def plot(x, y):
 def main():
     x, y = get_data("/Users/mostafasaber/Desktop/tempNLP/review_polarity/txt_sentoken")
 
-    tfid = TfidfVectorizer(lowercase=True, analyzer='word', stop_words='english')
-    x_train, y_train, x_test, y_test = set_matrices(x, y, int(0.75 * len(y)), tfid)
+    vec, x_train, y_train, x_test, y_test = set_matrices(x, y, int(0.75 * len(y)))
 
     logistic_regression = LogisticRegression(C=1e7)
     logistic_regression.fit(x_train, y_train)
@@ -40,12 +51,12 @@ def main():
 
     print(f"Accuracy score: {accuracy_score(y_test, predicted)}")
 
-    text_to_predict = ["This is a very bad movie", "This is a very good movie"]
+    text_to_predict = ["This is a great movie", "This is a very good movie"]
 
-    ans = predict(text_to_predict, logistic_regression, tfid)
+    ans = predict(text_to_predict, logistic_regression, vec)
     print(f"The predicted Label is: {ans}")
 
-    plot(x_train, y_train)
+    # plot(x_train, y_train)
 
 
 if __name__ == "__main__":
