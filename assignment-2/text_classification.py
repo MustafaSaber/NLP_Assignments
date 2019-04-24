f59804df8ad5a9d4e72472c9d4dce32219932094
Mostafa Saber <mostafa.saber3789@gmail.com>
2019-04-24 19:44:00 +0200

    sum embeddings

--- assignment-2/text_classification.py
+++ assignment-2/text_classification.py
@@ -7,27 +7,51 @@ from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 
 
-def set_matrices(x, y, to_split):
+def se_sum_of_we(x):
+    lines = open("glove.6B.100d.txt").readlines()
+    dic = {}
+    for i in lines:
+        values = i.split()
+        dic[values[:1]] = list(map(int, values[1:]))
+    list_of_vectors = []
+    for text in x:
+        words = text.lower().split()
+        temp_list_of_vectors = [dic[word] for word in words]
+        temp_list = [sum(t) for t in zip(*temp_list_of_vectors)]
+        list_of_vectors.append(temp_list)
+    # list of vectors is the new X
+    return list_of_vectors
+
+
+def tf_idf(x_train, x_test, tfid):
+    return tfid, tfid.fit_transform(x_train), tfid.transform(x_test)
+
+
+def set_matrices(x, y, to_split, algo):
+    tfid = TfidfVectorizer(lowercase=True, analyzer='word', stop_words='english')
     y_train, y_test = y[:to_split], y[to_split:]
     x_train, x_test = x[:to_split], x[to_split:]
-
-    def tf_idf():
-        tfid = TfidfVectorizer(lowercase=True, analyzer='word', stop_words='english')
-        return tfid, tfid.fit_transform(x_train), tfid.transform(x_test)
-
-    def se_sum_of_we():
-        lines = open("/assignment-2/glove.6B.100d.txt").readlines()
-        dic = {}
-        for i in lines:
-            values = i.split()
-            dic[values[:1]] = values[1:]
-
-    vec, x_train, x_test = tf_idf()
-    return vec, x_train, y_train, x_test, y_test
+    if algo == 0:
+        vec, x_train, x_test = tf_idf(x_train, x_test)
+        return tfid, x_train, y_train, x_test, y_test
+    if algo == 1:
+        new_x = se_sum_of_we(x)
+        return tfid, new_x[:to_split], y_train, new_x[to_split:], y_test
 
 
-def predict(text, model_used, vectroizer_used):
-    return model_used.predict(vectroizer_used.transform(text))
+def predict_tfidf(text, model_used, tf_idf):
+    return model_used.predict(tf_idf.transform(text))
+
+
+def predict_se_sum_of_we(text, model_used):
+    return model_used.predict(se_sum_of_we(text))
+
+
+def predict(text, model_used, tf_idf, algo):
+    if algo == 0:
+        return predict_tfidf(text, model_used, tf_idf)
+    if algo == 1:
+        return predict_se_sum_of_we(text, model_used)
 
 
 def plot(x, y):
@@ -43,7 +67,7 @@ def plot(x, y):
 def main():
     x, y = get_data("/Users/mostafasaber/Desktop/tempNLP/review_polarity/txt_sentoken")
 
-    vec, x_train, y_train, x_test, y_test = set_matrices(x, y, int(0.75 * len(y)))
+    vec, x_train, y_train, x_test, y_test = set_matrices(x, y, int(0.75 * len(y)), 1)
 
     logistic_regression = LogisticRegression(C=1e7)
     logistic_regression.fit(x_train, y_train)
@@ -53,7 +77,7 @@ def main():
 
     text_to_predict = ["This is a great movie", "This is a very good movie"]
 
-    ans = predict(text_to_predict, logistic_regression, vec)
+    ans = predict(text_to_predict, logistic_regression, vec, 1)
     print(f"The predicted Label is: {ans}")
 
     # plot(x_train, y_train)
